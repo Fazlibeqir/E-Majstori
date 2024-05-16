a2160aa1cbf2880422ecfcff5dff412492120c2c
Marko <markopetrov1212@gmail.com>
2024-05-17 00:46:53 +0200

    Edit migrations

--- backend/src/main/resources/db/migration/V3__create_job_provider.sql
+++ backend/src/main/resources/db/migration/V3__create_job_provider.sql
@@ -2,6 +2,8 @@ CREATE TABLE IF NOT EXISTS public.job_provider
 (
     id          BIGINT PRIMARY KEY NOT NULL,
     name        VARCHAR(255) NOT NULL,
+    image        VARCHAR(255),
+    phone_number        VARCHAR(255),
     description VARCHAR(255),
     location_id BIGINT
 );
