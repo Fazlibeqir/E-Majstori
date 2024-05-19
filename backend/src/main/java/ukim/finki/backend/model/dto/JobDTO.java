08ba7d8c6df3135c690286645c9d9b7e28da9ad3
Marko <markopetrov1212@gmail.com>
2024-05-19 22:19:24 +0200

    Edit JobDTO

--- backend/src/main/java/ukim/finki/backend/model/dto/JobDTO.java
+++ backend/src/main/java/ukim/finki/backend/model/dto/JobDTO.java
@@ -9,9 +9,6 @@ public class JobDTO {
     private String title;
     private String description;
     private double price;
-    private double grade;
-    private double total_grades;
-    private int number_reviews;
     private Long jobProviderId;
     private List<Long> categoryId;
 
@@ -22,9 +19,6 @@ public class JobDTO {
         this.title = title;
         this.description = description;
         this.price = price;
-        this.grade = 0.0;
-        this.total_grades = 0.0;
-        this.number_reviews = 0;
         this.jobProviderId = jobProviderId;
         this.categoryId = categoryId;
     }
